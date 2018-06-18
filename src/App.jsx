49251f31e1af3f22e9a5f61869da0b00bcffeb1e
Cook1619 <cook6348@gmail.com>
2018-06-18 18:27:33 -0500

    Setting up my first component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            movies: [],
+        }
+    }
+    componentDidMount = () => {
+        fetch('https://ghibliapi.herokuapp.com/films')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (myJson) {
+                console.log(myJson);
+            });
+    }
     render() {
         return (
-            <h1>Hello</h1>
+            <React.Fragment>
+            
+            </React.Fragment>
         )
     }
 }
