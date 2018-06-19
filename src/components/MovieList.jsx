5d201302b5bbf5d384b8b38687cbb49422efbb23
Cook1619 <cook6348@gmail.com>
2018-06-19 16:13:01 -0500

    Finally rendering some content

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import Card from './Card.jsx';
 
 const MovieList = (props) => {
-    let movies = props.info.map((movie, index) =>{
+    let movies = props.info.map((movie, index) => {
         return <Card key={index} movieList={movie} />
     })
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">{movies}</div>
+            </div>
+        </div>
+    )
 }
 
-return (
-    <div className="container">
-        <div className="row">
-            <div className="col">{movies}</div>
-        </div>
-    </div>
-)
+
+export default MovieList;
