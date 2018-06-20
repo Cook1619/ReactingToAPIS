b55ef1ebe8b1ae7eda62a92e9c4a873b4088c123
Cook1619 <cook6348@gmail.com>
2018-06-20 18:12:03 -0500

    same issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import List from './components/List';
+import List from './components/List.jsx';
 import './styles.css';
 
 const Base_URL = 'https://ghibliapi.herokuapp.com/films';
